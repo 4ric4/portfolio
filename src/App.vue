e22997e1d70c0f1c2be6714c25301c3957d0bfda
Ricardo <ricardocavalcantecs@gmail.com>
2025-03-21 12:29:01 -0300

    Feat:acrescentando experiencia e skill

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
     <NavBar />
     <HeroSection />
     <ServicesSection />
+    <AboutSection />
+    <ExperienceAndSkills />
+    <LatestProjSecstion />
+
   </div>
 
 </template>
@@ -11,9 +15,11 @@
 
 <script setup>
 import { defineAsyncComponent } from 'vue'
-
 const NavBar = defineAsyncComponent(() => import('@/components/NavBar.vue'))
 const HeroSection = defineAsyncComponent(() => import('@/components/HeroSection.vue'))
 const ServicesSection = defineAsyncComponent(() => import('@/components/ServicesSection.vue'))
+const AboutSection = defineAsyncComponent(() => import('@/components/AboutSection.vue'))
+const ExperienceAndSkills = defineAsyncComponent(() => import('@/components/ExperienceAndSkills.vue'))
+const LatestProjSecstion = defineAsyncComponent(() => import('@/components/LatestProjSecstion.vue'))
 
 </script>
